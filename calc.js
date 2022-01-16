11fbf84a3f818278273a0ba70882f22769cd8698
Joshua Boyd <jaboydfamily@gmail.com>
2022-01-16 09:45:59 -0500

    adjusted discription font - got basic calc functions to work

--- calc.js
+++ calc.js
@@ -26,10 +26,10 @@ const calculator = {
 			case '-':
 				result = this.sub(a, b);
 				break;
-			case '*':
+			case 'x':
 				result = this.mul(a, b);
 				break;
-			case '/':
+			case String.fromCharCode(247):
 				result = this.div(a, b);
 				break;
 			default:
@@ -48,7 +48,7 @@ let numButtons = document.getElementsByClassName('calButton');
 for(let i=0; i<numButtons.length; i++) {
 	numButtons[i].addEventListener('click', (e) => {
 		let exprDisplay = document.querySelector('#exprDisplay');
-		exprDisplay.innerHTML = `${exprDisplay.innerHTML} ${numButtons[i].innerHTML}`;
+		exprDisplay.innerHTML = `${exprDisplay.innerHTML}${numButtons[i].innerHTML}`;
 	});
 }
 
@@ -57,10 +57,59 @@ let opButtons = document.getElementsByClassName('opButton');
 for(let i=0; i<opButtons.length; i++) {
 	opButtons[i].addEventListener('click', (e) => {
 		let exprDisplay = document.querySelector('#exprDisplay');
+		let resultDisplay = document.querySelector('#resultDisplay');
 		if(opButtons[i].innerHTML === 'AC') {
 			exprDisplay.innerHTML = '';
+			resultDisplay.innerHTML = '';
+		}else if(opButtons[i].innerHTML === 'C') {
+			exprDisplay.innerHTML = exprDisplay.innerHTML.slice(0, -1);
 		}else {
-			exprDisplay.innerHTML = `${exprDisplay.innerHTML} ${opButtons[i].innerHTML}`;
+			exprDisplay.innerHTML = `${exprDisplay.innerHTML}${opButtons[i].innerHTML}`;
 		}
 	});
 }
+
+let equalButton = document.querySelector('#equalButton');
+equalButton.addEventListener('click', (e) => {
+	let exprDisplay = document.querySelector('#exprDisplay');
+	let resultDisplay = document.querySelector('#resultDisplay');
+	let expressArr;
+	let result;
+
+	if(exprDisplay.innerHTML.includes('+')) {
+
+		expressArr = exprDisplay.innerHTML.trim().split('+');
+		if(expressArr.length === 2) {
+			exprDisplay.innerHTML = `${exprDisplay.innerHTML}=`;
+		}
+		result = calculator.operate('+', parseInt(expressArr[0]), parseInt(expressArr[1]));
+
+	}else if(exprDisplay.innerHTML.includes('-')) {
+
+		expressArr = exprDisplay.innerHTML.trim().split('-');
+		if(expressArr.length === 2) {
+			exprDisplay.innerHTML = `${exprDisplay.innerHTML}=`;
+		}
+		result = calculator.operate('-', parseInt(expressArr[0]), parseInt(expressArr[1]));
+
+	}else if(exprDisplay.innerHTML.includes('x')) {
+
+		expressArr = exprDisplay.innerHTML.trim().split('x');
+		if(expressArr.length === 2) {
+			exprDisplay.innerHTML = `${exprDisplay.innerHTML}=`;
+		}
+		result = calculator.operate('x', parseInt(expressArr[0]), parseInt(expressArr[1]));
+
+	}else if(exprDisplay.innerHTML.includes(String.fromCharCode(247))) {
+
+		expressArr = exprDisplay.innerHTML.trim().split(String.fromCharCode(247));
+		if(expressArr.length === 2) {
+			exprDisplay.innerHTML = `${exprDisplay.innerHTML}=`;
+		}
+		result = calculator.operate(String.fromCharCode(247), 
+									parseInt(expressArr[0]), parseInt(expressArr[1]));
+
+	}
+
+	resultDisplay.innerHTML = result;
+});
